8f76f29aec49f6e05deb979e63e6bdbc479811e2
jesusrlv <jesusrlvrojo@gmail.com>
2025-02-12 11:55:00 -0600

    iniciar proceso abrir modal

--- sistema/script.js
+++ sistema/script.js
@@ -2,3 +2,8 @@ function zoomIn() {
     let miDiv = document.getElementById('lotes');
     miDiv.style.transform = 'scale(1.5)'; /* Restablece el tamaño al 100% */
 }
+function zoomOut() {
+    let miDiv = document.getElementById('lotes');
+    miDiv.style.transform = 'scale(1)'; /* Restablece el tamaño al 100% */
+}
+
