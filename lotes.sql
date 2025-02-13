1a985df47e1521e7d0ebf4bf8109d420fce533aa
jesusrlv <jesusrlvrojo@gmail.com>
2025-02-13 12:25:26 -0600

    db

--- lotes.sql
+++ lotes.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 12-02-2025 a las 23:28:57
+-- Tiempo de generación: 13-02-2025 a las 19:22:49
 -- Versión del servidor: 10.4.21-MariaDB
--- Versión de PHP: 7.4.28
+-- Versión de PHP: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,15 +36,23 @@ CREATE TABLE `lote` (
   `manzana` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `lote` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `calle` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
-  `superficie` varchar(30) COLLATE utf8_unicode_ci NOT NULL
+  `superficie` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `estatus` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `lote`
 --
 
-INSERT INTO `lote` (`id`, `capa`, `nombre`, `direccion`, `telefono`, `manzana`, `lote`, `calle`, `superficie`) VALUES
-(1, 'Capa_782', 'JEsusRlv', 'Tulipanes 12 a', '4915000', '3', '5', 'Conocida', '100mts');
+INSERT INTO `lote` (`id`, `capa`, `nombre`, `direccion`, `telefono`, `manzana`, `lote`, `calle`, `superficie`, `estatus`) VALUES
+(1, 'Capa_782', 'JEsusRlv', 'Tulipanes 12 a', '4915000', '3', '5', 'Conocida', '100mts', 1),
+(2, 'Capa_288', 'Jesusrlv', 'Tulipanes 12 a', '4915000', '12', '1234', 'Tulipanes', '12', 0),
+(3, 'Capa_293', 'w', 'w', 'w', '3', 'w', 'w', '2', 1),
+(4, 'Capa_2', 'no tiene aún', 'si tiene', '999999', 'Datos nuevos', '1', 'calee', '23', 1),
+(5, 'Capa_9', 'dsds', 'dsd', 'sdsd', 'sds', 'sdsd', 'sds', 'sdsd', 1),
+(6, 'Capa_24', '2', '2', '2', 'datos', '2', '2', '2', 0),
+(7, 'Capa_228', 'q', 'q', 'q', 'q', 'q', 'q', 'q', 0),
+(8, 'Capa_125', 'Rodolfo Leaños', 'Tulipanes 12 a', '9236222', '7', '9', 'Calle x', '70mts', 1);
 
 -- --------------------------------------------------------
 
@@ -90,7 +98,7 @@ ALTER TABLE `usr`
 -- AUTO_INCREMENT de la tabla `lote`
 --
 ALTER TABLE `lote`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `usr`
